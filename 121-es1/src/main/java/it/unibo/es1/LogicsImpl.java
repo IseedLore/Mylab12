ec21fa52bba0457f853ddcd64e8271b7c4a19959
Lorenzo Ferri - lorenzo.ferri21@studio.unibo.it <lorenzo.ferri21@studio.unibo.it>
2024-12-09 12:34:13 +0100

    All done 121-es1

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,60 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogicsImpl implements Logics {
 
+	private final int size;
+	private final List<Integer> values;
+	private final List<Boolean> enablings;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.size = size;
+		this.values = new ArrayList<Integer>(this.size);
+		this.enablings = new ArrayList<Boolean>(this.size);
+		for(int i = 0; i < size; i++) {
+			this.values.add(0);
+		}
+		for(int i = 0; i < size; i++) {
+			this.enablings.add(true);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return new ArrayList<>(this.values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return new ArrayList<>(this.enablings);
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		int value = this.values.get(elem) +1;
+		this.values.remove(elem);
+		this.values.add(elem, value);
+		if(value == this.size){
+			this.enablings.remove(elem);
+			this.enablings.add(elem, false);
+		}
+		return value;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.values.toString().replaceAll(", ", "|").replace("[","<<").replace("]",">>");
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return this.values.stream().distinct().count() == 1;
 	}
 }
